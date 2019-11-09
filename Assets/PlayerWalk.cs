5f2a9a1d1a09e729dd107aa1b1001c27e6ef00e0
jdanovvictor <zhdanov.viktor.it@yandex.ru>
2019-11-09 14:11:25 +0200

    First commit

---
+++ Assets/PlayerWalk.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int playerSpeed;
+    public bool isGrounded;
+
+
+
+
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetButton("Jump") && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 3, 0), ForceMode.Impulse);
+            isGrounded = false;
+
+        }
+
+    }
+
+}
