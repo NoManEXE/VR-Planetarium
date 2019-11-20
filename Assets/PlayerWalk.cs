b40ebacbbd13215f66bc53eadbfaba15e5493250
Dagonail <Dagonail@mail.ru>
2019-11-20 16:01:15 +0200

    fix+walk

--- Assets/PlayerWalk.cs
+++ Assets/PlayerWalk.cs
@@ -8,8 +8,7 @@ public class PlayerWalk : MonoBehaviour
     public int playerSpeed;
     public bool isGrounded;
 
-
-
+  
 
 
     Rigidbody rb;
@@ -30,12 +29,34 @@ public class PlayerWalk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetKey(KeyCode.W) || Input.GetButton("Fire1") || Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+            transform.position += Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                playerSpeed += 3;
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                playerSpeed -= 3;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Camera.main.transform.right * playerSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= Camera.main.transform.right * playerSpeed * Time.deltaTime;
         }
 
-        if(Input.GetButton("Jump") && isGrounded)
+
+        if (Input.GetButton("Jump") && isGrounded)
         {
             rb.AddForce(new Vector3(0, 3, 0), ForceMode.Impulse);
             isGrounded = false;
