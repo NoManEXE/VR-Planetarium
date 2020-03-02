af59c86b99207e41e1984f182bf6e5e8fcba6b03
NoManEXE <weidmusic@gmail.com>
2020-03-02 22:46:07 +0200

    some improvements for the menu

--- Assets/LowPolyEarth/EarthSpinScript.cs
+++ Assets/LowPolyEarth/EarthSpinScript.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class EarthSpinScript : MonoBehaviour {
     public float speed = 10f;
+    public bool rotateX;
 
     void Update() {
-        transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+        if (rotateX == true)
+        {
+            transform.Rotate(Vector3.right, speed * Time.deltaTime, Space.World);
+        }
+
+        else
+        {
+            transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+        }
     }
 }
