75966eb61867800533f2454f363192fabb7d7e79
NoManEXE <weidmusic@gmail.com>
2020-03-28 20:42:15 +0200

    added testing termonal with planet information and records table

---
+++ Assets/Player/BasePlayerClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePlayerClass : MonoBehaviour
+{
+    private string username;
+    private int score;
+    private int quizzesPassedNum;
+    private int quizzesExecuted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ShowMessageOnEnter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShowMessageOnEnter : MonoBehaviour
+{
+    public GameObject welcomeMessage;
+    public float time = 5; //Seconds to read the text
+
+    private void OnTriggerEnter(Collider myCollider)
+    {
+        if(myCollider.tag == ("Player"))
+        {
+            Debug.Log("Trigger executed");
+            welcomeMessage.SetActive(true);
+            
+
+            Debug.Log("object found and set active");
+            Destroy(welcomeMessage, time);
+        }
+    }
+}
