a91b3b5be7754f2ef027c92bf2522df91d8af817
Dagonail <Dagonail@mail.ru>
2020-04-24 23:38:45 +0300

    Planets_Solar_System

---
+++ Assets/Scipts/RotationAroundSun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationAroundSun : MonoBehaviour
+{
+
+    int factor = 12;
+    public long timeAround = 31536000;
+    public Transform origin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(origin.position, Vector3.down, 360.0f / (timeAround/factor) * Time.deltaTime);
+    }
+}
